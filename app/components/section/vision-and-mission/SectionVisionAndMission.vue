8c029cf27ed817c5bc5945b0242ae04b3d853c73
dioauliaharrisa <daharrisa@gmail.com>
2025-10-29 10:56:02 +0700

    'update'

--- app/components/section/vision-and-mission/SectionVisionAndMission.vue
+++ app/components/section/vision-and-mission/SectionVisionAndMission.vue
@@ -1,14 +1,10 @@
 <template>
   <p class="p-8 leading-6 text-center">
-    <strong> Riichi Indonesia </strong> has always wanting to make an
-    international scale riichi tournament, and finally stars aligned, making it
-    happen with the
+    <strong> Riichi Indonesia </strong> {{ $t("vision_mission_1st_sentence") }}
     <strong>
-      Indonesia International Invitational Tournament of Riichi Mahjong 2026 </strong
-    >. We believe that we need more international tournament which spans for a
-    longer period of time to test consistency and grit of skillful players. We
-    also want to let the players to pour some time as commentator to share their
-    own view towards the game.
-    <strong> May the winner be the best player! </strong>
+      Indonesia International Invitational Tournament of Riichi Mahjong 2026.
+    </strong>
+    {{ $t("vision_mission_2nd_sentence") }}
+    <strong> {{ $t("vision_mission_3rd_sentence") }} </strong>
   </p>
 </template>
