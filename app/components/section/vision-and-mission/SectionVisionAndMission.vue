b76740500e27a1c3de34426063f19685d4a39936
dioauliaharrisa <daharrisa@gmail.com>
2025-10-30 01:32:33 +0700

    'update'

--- app/components/section/vision-and-mission/SectionVisionAndMission.vue
+++ app/components/section/vision-and-mission/SectionVisionAndMission.vue
@@ -1,10 +1,5 @@
 <template>
   <p class="p-8 leading-6 text-center text-xs">
-    <strong> Riichi Indonesia </strong> {{ $t("vision_mission_1st_sentence") }}
-    <strong>
-      Indonesia International Invitational Team League 2026.
-    </strong>
-    {{ $t("vision_mission_2nd_sentence") }}
-    <strong> {{ $t("vision_mission_3rd_sentence") }} </strong>
+    {{ $t("vision_mission_sentence") }}
   </p>
 </template>
