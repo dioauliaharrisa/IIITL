328f43c8c6afe6b795293cec4534c389c3be4cd1
dioauliaharrisa <daharrisa@gmail.com>
2025-11-03 02:38:01 +0700

    'update'

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,15 @@
+<script setup>
+import PublicGoogleSheetsParser from "public-google-sheets-parser";
+const options = { sheetName: "Display_Stream_Schedule", useFormat: true };
+const parser = new PublicGoogleSheetsParser(
+  "1EJxSdz98HHM3gPD9u7fjiLWWmu_ZDBV0U1m-Z-a1uGc",
+  options
+);
+parser.parse().then((data) => {
+  console.log("🦆 ~ data:", data);
+});
+</script>
+
 <template>
   <div>
     <UMain>
