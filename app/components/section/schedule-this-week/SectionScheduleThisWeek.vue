71d9a4ac1b5bb8bee7bd777bc5dbd112badf5448
dioauliaharrisa <daharrisa@gmail.com>
2025-11-05 14:12:30 +0700

    'update'

---
+++ app/components/section/schedule-this-week/SectionScheduleThisWeek.vue
@@ -0,0 +1,49 @@
+<script setup>
+import PublicGoogleSheetsParser from "public-google-sheets-parser";
+
+const schedule = ref([]);
+const options = { sheetName: "Display_Stream_Schedule", useFormat: true };
+const parser = new PublicGoogleSheetsParser(
+  "1EJxSdz98HHM3gPD9u7fjiLWWmu_ZDBV0U1m-Z-a1uGc",
+  options
+);
+parser.parse().then((data) => {
+  schedule.value = data;
+  console.log("🦆 ~ schedule:", schedule.value);
+});
+</script>
+
+<template>
+  <div class="min-h-[200px]">
+    <UCarousel
+      v-slot="{ item }"
+      dots
+      :items="schedule"
+      class="w-full max-w-xs mx-auto"
+    >
+      <pre class="font-semibold">{{ item.schedule }}</pre>
+      <div class="grid grid-cols-2 gap-4 text-xs h-[375px]">
+        <UCard
+          v-for="key in item.listCountries.split(',')"
+          :key="key"
+          variant="solid"
+          class="flex flex-col justify-between"
+          :ui="{
+            body: 'p-0',
+            footer: 'p-2 bg-amber-600',
+          }"
+        >
+          <NuxtImg
+            :src="'https://i.imgur.com/hChfMhT.png'"
+            alt="Country Flag"
+            class="mx-auto"
+          />
+
+          <template #footer>
+            <p class="text-center">{{ key }}</p>
+          </template>
+        </UCard>
+      </div>
+    </UCarousel>
+  </div>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,18 +1,7 @@
-<script setup>
-import PublicGoogleSheetsParser from "public-google-sheets-parser";
-const options = { sheetName: "Display_Stream_Schedule", useFormat: true };
-const parser = new PublicGoogleSheetsParser(
-  "1EJxSdz98HHM3gPD9u7fjiLWWmu_ZDBV0U1m-Z-a1uGc",
-  options
-);
-parser.parse().then((data) => {
-  console.log("🦆 ~ data:", data);
-});
-</script>
-
 <template>
   <div>
     <UMain>
+      <SectionScheduleThisWeek />
       <!-- <MarqueeFlags /> -->
       <SectionVisionAndMission />
       <LazySectionCheckCurrentStanding />
