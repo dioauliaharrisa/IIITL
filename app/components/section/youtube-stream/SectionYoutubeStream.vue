8631b014ae5e90518deb126aa83740453db9357f
dioauliaharrisa <daharrisa@gmail.com>
2026-01-06 23:04:36 +0700

    'update'

---
+++ app/components/section/youtube-stream/SectionYoutubeStream.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="relative w-full" style="padding-top: 56.25%">
+    <iframe
+      class="absolute top-0 left-0 w-full h-full"
+      :src="`https://www.youtube.com/embed/9WZT8RhXO74`"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+      allowfullscreen
+    ></iframe>
+    <!-- allowfullscreen -->
+  </div>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,12 @@
+<script setup lang="ts">
+import SectionYoutubeStream from "../../app/components/section/youtube-stream/SectionYoutubeStream.vue";
+</script>
+
 <template>
   <div>
     <UMain>
       <SectionScheduleThisWeek />
+      <SectionYoutubeStream />
       <MarqueeFlags />
       <SectionVisionAndMission />
       <LazySectionCheckCurrentStanding />
