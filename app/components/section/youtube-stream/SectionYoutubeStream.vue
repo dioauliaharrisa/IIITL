bf8072a9002181579fcde104184482799ae17d3d
dioauliaharrisa <daharrisa@gmail.com>
2026-01-23 16:50:52 +0700

    'update'

--- app/components/section/youtube-stream/SectionYoutubeStream.vue
+++ app/components/section/youtube-stream/SectionYoutubeStream.vue
@@ -2,9 +2,17 @@
   <div class="relative w-full" style="padding-top: 56.25%">
     <iframe
       class="absolute top-0 left-0 w-full h-full"
-      :src="`https://www.youtube.com/embed/l9TqEz8sfAM`"
+      :src="`https://www.youtube.com/embed/Svf7xuZi2Dw`"
       frameborder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+      allow="
+        accelerometer;
+        autoplay;
+        clipboard-write;
+        encrypted-media;
+        gyroscope;
+        picture-in-picture;
+        fullscreen;
+      "
       allowfullscreen
     ></iframe>
     <!-- allowfullscreen -->
