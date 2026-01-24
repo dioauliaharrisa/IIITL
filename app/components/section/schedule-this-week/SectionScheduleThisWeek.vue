32852746cd004e924bfc11bdbe4ae5eeb02f9bab
dioauliaharrisa <daharrisa@gmail.com>
2026-01-24 17:18:23 +0700

    'update'

--- app/components/section/schedule-this-week/SectionScheduleThisWeek.vue
+++ app/components/section/schedule-this-week/SectionScheduleThisWeek.vue
@@ -9,7 +9,7 @@ const schedule = ref([]);
 const options = { sheetName: "Display_Stream_Schedule", useFormat: true };
 const parser = new PublicGoogleSheetsParser(
   "1EJxSdz98HHM3gPD9u7fjiLWWmu_ZDBV0U1m-Z-a1uGc",
-  options
+  options,
 );
 parser.parse().then((data) => {
   schedule.value = data.slice(0, 6);
@@ -30,31 +30,25 @@ parser.parse().then((data) => {
     >
       <p class="font-semibold text-xl py-2">{{ item.schedule }}</p>
       <div class="grid grid-cols-2 gap-4 text-xs h-[375px]">
-        <UCard
+        <div
           v-for="key in item.listCountries?.split(',').map((k) => k.trim())"
           :key="key"
-          variant="solid"
-          class="flex flex-col justify-between"
-          :ui="{
-            body: 'p-0',
-            footer: 'p-2 bg-red-500',
-          }"
+          class="flex flex-col overflow-hidden rounded-lg bg-[#0b132b] cursor-pointer"
           @click="$router.push(`/team/${key}`)"
         >
-          <NuxtImg
-            :src="teamMap[key] || ''"
-            format="webp"
-            alt="Logo"
-            class="mx-auto"
-            width="200"
-            height="170"
-            quality="70"
-            :modifiers="{ w: 150, h: 150 }"
-          />
-          <template #footer>
-            <p class="text-center">{{ key }}</p>
-          </template>
-        </UCard>
+          <div class="w-full aspect-square overflow-hidden">
+            <NuxtImg
+              :src="teamMap[key] || ''"
+              class="w-full h-full object-cover"
+              format="webp"
+              quality="70"
+            />
+          </div>
+
+          <div class="p-2 bg-red-500 text-center">
+            <p class="text-xs text-white">{{ key }}</p>
+          </div>
+        </div>
       </div>
     </UCarousel>
   </div>
